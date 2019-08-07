59e70afd61fd5db5233bb08b009da92a5ff2ed1c
aviadsolganik <50738644+aviadsolganik@users.noreply.github.com>
2019-08-07 19:57:53 +0300

    beautiful site
    beautiful site

--- SportGames/App_Start/BundleConfig.cs
+++ SportGames/App_Start/BundleConfig.cs
@@ -22,9 +22,13 @@ namespace SportGames
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css","~/Content/a.css"));
+
         }
     }
 }
--- SportGames/Controllers/HomeController.cs
+++ SportGames/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace SportGames.Controllers
 
             return View();
         }
+
+        public ActionResult Game()
+        {
+            return View();
+        }
     }
 }
